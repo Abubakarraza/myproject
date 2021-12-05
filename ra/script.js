dc76f6a94c99d161379e140cb66a3b8b89881f2b
Abubakarraza <95075448+Abubakarraza@users.noreply.github.com>
2021-12-06 00:48:03 +0500

    Add files via upload

---
+++ ra/script.js
@@ -0,0 +1,17 @@
+function raza() {
+    var a = document.getElementById("a").value
+    var b = document.getElementById("output")
+    output.innerHTML = a}
+  var num1 = 8
+var num2 = 2
+var num3 = num1/ num2
+//function raza() {
+  //  var num3 = num1/ num2
+   // console.log("number is even",num3);
+    
+//}
+raza()
+if (num1/num2) {
+    var num3
+    
+}
