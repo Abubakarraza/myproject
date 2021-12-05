422ea61cfb1ff980c1d2c9514fa34a36600fe11a
Abubakarraza <95075448+Abubakarraza@users.noreply.github.com>
2021-12-06 01:12:23 +0500

    Add files via upload

---
+++ d2/index.js
@@ -0,0 +1,136 @@
+//var name="abubakar"
+//console.log(name);
+//var num1=25
+//var num2=5
+//var sum=num1+num2
+//var sub=num1-num2
+//var div=num1/num2
+//ar mult=num1*num2
+//console.log(sum);
+//console.log(sub);
+//console.log(div);
+//console.log(mult);
+//var nameOfId=234
+//console.log("nameOfId",typeof(nameOfId));
+//typeof(num1)
+//var num1="25"
+//var num2=5
+//var num3=6
+//num1= parseInt(num1)
+//var sum=num2+num3+num1
+//console.log("sum",sum);
+//var num4=45
+//var num5=45
+//var result1=num4==num5
+//var result2=num4!=num5
+//console.log("result1",result1);
+//console.log("result2",result2);
+//var num6=45
+//var num7=47
+//var result3=num6<num7||num6>num7
+//var result4=num6>num7
+
+//console.log("result4",result4);
+//if(6==7){
+  //  console.log("result0");
+//}
+//else{
+//    console.log("result01");
+//}
+//var score= 95
+//if (score>50 & score<60) {
+//    console.log("student grade is D");
+//}
+//if (score>=60 & score<70) {
+  //  console.log("student grade is C");
+//}
+//if (score>=70 & score<80) {
+  //  console.log("student grade is B");
+//}
+//if (score>=80 & score<90) {
+  //  console.log("student grade is A");
+//}
+
+//if (score>90 & score<100) {
+ //   console.log("student grade is A+");
+//}
+
+
+
+var day = "mon"
+switch (day) {
+  case "mon":
+    console.log("today is monday");
+    break;
+    case "tue":
+    console.log("today is tue");
+    break;
+    case "wed":
+    console.log("today is wed");
+    break;
+    case "thu":
+    console.log("today is thu");
+    break;
+    case "fri":
+    console.log("today is fri");
+    break;
+    case "sat":
+    console.log("today is sat");
+    break;
+    case "sun":
+    console.log("today is sun");
+    break;
+
+
+  default:
+    break;
+}
+var day = 2
+switch (day) {
+  case 1:
+    console.log("today is monday");
+    break;
+    case 2:
+    console.log("today is tue");
+    break;
+    case 3:
+    console.log("today is wed");
+    break;
+    case 4:
+    console.log("today is thu");
+    break;
+    case 5:
+    console.log("today is fri");
+    break;
+    case 6:
+    console.log("today is sat");
+    break;
+    case 7:
+    console.log("today is sun");
+    break;
+
+
+  default:
+    break;
+}
+//loop
+console.log("before loop");
+for (let index = 0; index < 67; index++) {
+  console.log( "loop console");
+}
+console.log("before loop");
+
+
+
+
+//array
+var numbers = [1,2,3,4,5,6,7,8,9]
+var names = ["ma","abubakar","raza"]
+console.log(numbers[0]);
+console.log(names[2]);
+console.log(numbers.length);
+console.log(names[names.length-2]);
+for (let index = 0; index < names.length; index++) {
+console.log(names[index]);
+  
+}
